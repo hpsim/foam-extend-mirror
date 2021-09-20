64d0cf12a485982c9762d53fb70aa3bc42a0fa4e
Hrvoje Jasak <h.jasak@wikki.co.uk>
2021-09-20 09:24:18 +0100

    Clean-up

--- src/foam/meshes/polyMesh/polyPatches/constraint/processor/processorPolyPatch.C
+++ src/foam/meshes/polyMesh/polyPatches/constraint/processor/processorPolyPatch.C
@@ -220,7 +220,6 @@ void Foam::processorPolyPatch::calcGeometry()
         // Calculate normals from areas and check
 
         // Cache face areas
-        // const vectorField::subField localFaceAreas = faceAreas();
         const vectorField::subField localFaceAreas = faceAreas();
 
         forAll (faceNormals, facei)
